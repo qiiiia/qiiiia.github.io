341ddd376d7a0580d6da755a4f65a0ea970e9bfa
qiiiia <1938995499@qq.com>
2025-07-26 21:56:55 +0800

    上传site.config

--- src/site.config.ts
+++ src/site.config.ts
@@ -3,7 +3,7 @@ import type { AstroExpressiveCodeOptions } from "astro-expressive-code";
 
 export const siteConfig: SiteConfig = {
 	// Used as both a meta property (src/components/BaseHead.astro L:31 + L:49) & the generated satori png (src/pages/og-image/[slug].png.ts)
-	author: "QI Z",
+	author: "QIIIIA",
 	// Date.prototype.toLocaleDateString() parameters, found in src/utils/date.ts.
 	date: {
 		locale: "zh-CN",
@@ -14,15 +14,15 @@ export const siteConfig: SiteConfig = {
 		},
 	},
 	// Used as the default description meta property and webmanifest description
-	description: "Qi Z's blog, a place for my thoughts and ideas.",
+	description: "QIIIIA's blog, a place for my thoughts and ideas.",
 	// HTML lang property, found in src/layouts/Base.astro L:18 & astro.config.ts L:48
 	lang: "zh-CN",
 	// Meta property, found in src/components/BaseHead.astro L:42
 	ogLocale: "zh_CN",
 	// Used to construct the meta title property found in src/components/BaseHead.astro L:11, and webmanifest name found in astro.config.ts L:42
-	title: "Qi Z",
+	title: "QIIIIA",
 	// ! Please remember to replace the following site property with your own domain, used in astro.config.ts
-	url: "https://qi-xmu.github.io",
+	url: "https://qiiiia.github.io",
 };
 
 // Used to generate links in both the Header & Footer.
